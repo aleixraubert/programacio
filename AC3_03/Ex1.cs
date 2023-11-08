e160fb87868bcc83bf7c1f9a8239112f0408af55
Aleix Raubert <146190136+aleixraubert@users.noreply.github.com>
2023-11-08 14:02:28 +0100

    Add files via upload

---
+++ AC3_03/Ex1.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Author: Aleix Raubert
+ * M03. Programació UF1
+ * v1. 8/1/2023
+ * Exercici 1. Implementa un programa que demani a l’usuari quants valors vol introduir i el programa generi una llista, a partir de valors introduïts per teclat, retornant la suma de tots els elements d’aquesta.
+ *
+ */
+
+
+
+using System;
+using System.Xml.Schema;
+
+namespace prog
+{
+    class Repte_10_Alter
+    {
+        static void Main()
+        {
+            const string MSG_Welcome = "Quants valors vols introduir?:";
+            const string MSG_Do = "Dons vinga, introduerix-los:";
+            const string MSG_Final = "La suma és {0}.";
+
+            Console.WriteLine(MSG_Welcome);
+
+            int size=Convert.ToInt32(Console.ReadLine());
+
+            int sum=0;
+
+            int[] nums = new int[size];
+
+            Console.WriteLine(MSG_Do);
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i]=Convert.ToInt32(Console.ReadLine());
+            }
+
+            foreach(int i in nums)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine(MSG_Final, sum);
+        }
+    }
+}
---
+++ AC3_03/Ex2.cs
@@ -0,0 +1,64 @@
+﻿/*
+ * Author: Aleix Raubert
+ * M03. Programació UF1
+ * v1. 8/1/2023
+ * Exercici 2. Donada la següent llista: a = [10,20,30,20,10,50,60,40,80,50,40] implementa un programa que retorni una llista sense els elements duplicats.
+ *
+ */
+
+
+
+using System;
+using System.Xml.Schema;
+
+namespace prog
+{
+    class Repte_10_Alter
+    {
+        static void Main()
+        {
+            const string MSG_Welcome = "Tenim aquesta llista: a = [10,20,30,20,10,50,60,40,80,50,40].";
+            const string MSG_Final = "Aquí està la llista sense elements duplicats.";
+
+
+            Console.WriteLine(MSG_Welcome);
+
+            int[] nums = new int[] { 10, 20, 30, 20, 10, 50, 60, 40, 80, 50, 40 };
+
+            int[] no_rep = new int[nums.Length];
+
+            for(int i=0; i<nums.Length-1; i++)
+            {
+                for(int j=i+1; j<nums.Length; j++)
+                {
+                    if (nums[j] < nums[i])
+                    {
+                        int aux = nums[j];
+                        nums[j] = nums[i];
+                        nums[i] = aux;
+                    }
+                }
+            }
+
+            int k = 0;
+
+            for(int i=0 ; i<nums.Length-1 ; i++)
+            {
+                if (nums[i] != nums[i + 1])
+                {
+                    no_rep[k] = nums[i];
+                    k++;
+                }
+
+            }
+
+            Console.WriteLine(MSG_Final);
+
+            foreach(int i in no_rep)
+            {
+                if(i!=0) Console.Write(i + " ");
+            }
+        }
+    }
+}
+
