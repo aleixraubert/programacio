4f14191b621e4a0397452a60e2de3e890092ed23
Aleix Raubert <146190136+aleixraubert@users.noreply.github.com>
2023-11-08 12:01:17 +0100

    Add files via upload

---
+++ Arrays/ex30.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Xml.Schema;
+
+namespace prog
+{
+    class Program
+    {
+        static void Main()
+        {
+            string membres = Console.ReadLine();
+            string[] noms = membres.Split(", ");
+            string[] noms_2 = membres.Split(" i ");
+            //string inicials = "";
+
+            foreach (string nom in noms)
+            {
+                //Replace de Acentos
+                //inicials += nom[0];
+                Console.Write(nom[0]);
+            }
+
+            //Replace de Acentos
+            //inicials += noms_2[1][0];
+            Console.Write(noms_2[1][0]);
+
+
+            //Console.WriteLine(inicials);
+        }
+    }
+}
